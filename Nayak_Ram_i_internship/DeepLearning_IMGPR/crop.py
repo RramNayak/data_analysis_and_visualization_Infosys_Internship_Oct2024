d80e6b437a0271cb028f4e2edd1dfd09f193d39a
Ram Nayak <130270545+RramNayak@users.noreply.github.com>
2024-12-03 21:12:11 +0530

    Delete Nayak_Ram_i_internship directory

--- Nayak_Ram_i_internship/DeepLearning_IMGPR/crop.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('C:/Users/nr/OneDrive/Desktop/Infosys/Task3/demo.jpg')
-cropped = img[50:200, 100:300]
-
-cv2.imshow('Cropped Image', cropped)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
